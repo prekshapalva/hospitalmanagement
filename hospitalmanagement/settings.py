16af11595f3b511a90140c8086b0db7487b0c367
prekshapalva <prekshap.is19@sahyadri.edu.in>
2020-09-22 20:17:41 +0530

    hospitalmanagement

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -109,9 +109,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ' '
+EMAIL_HOST_USER = 'hospitalmanagement@gmail.com '
 EMAIL_HOST_PASSWORD = ' '
 EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
