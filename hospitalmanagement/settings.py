f64f490b5e305611ea5f4b27b62baad102f29eb1
prekshapalva <prekshap.is19@sahyadri.edu.in>
2020-09-23 21:11:55 +0530

    hospitalmanagement

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -113,7 +113,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hospitalmanagement81@gmail.com'
-EMAIL_HOST_PASSWORD = 'hospitaladmin'
+EMAIL_HOST_PASSWORD = '==='
 EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
