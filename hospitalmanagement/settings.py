c04ad7f716854d29db6232a2d18749a82cbab249
prekshapalva <prekshap.is19@sahyadri.edu.in>
2020-10-08 18:02:09 +0530

    procfile

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -113,15 +113,18 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hospitalmanagement81@gmail.com'
-EMAIL_HOST_PASSWORD = '==='
+EMAIL_HOST_PASSWORD = 'hospitaladmin'
 EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR,'static')
+#]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 APPEND_SLASH = False
 
