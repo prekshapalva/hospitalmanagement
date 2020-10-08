defe34b7fdac9804e6525ba5fc7407d4658d5e97
prekshapalva <prekshap.is19@sahyadri.edu.in>
2020-10-08 18:58:44 +0530

    settings

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import os
 
+import os
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,6 @@ APPEND_SLASH = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
